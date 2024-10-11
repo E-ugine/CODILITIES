8ea6032e06ed21381a5bcd8a38c4cbbca4ede798
EugineAgolla <agollaeugine@gmail.com>
2024-10-11 19:52:19 +0300

    updates debug file

--- functions.js
+++ functions.js
@@ -19,4 +19,16 @@ console.log(max5(1,2,3,7,8))
 function minutesToHours(minutes){
    return (minutes/60)
 }
-console.log(minutesToHours(150))
+console.log(minutesToHours(200))
+
+//Write a function named canDriveCar that receives 2 parameters:
+
+// an object named user
+// an object named car
+// and returns a boolean indicating if the user can drive the car or not.
+
+// Note: anybody can drive a car if it has at least 18 years old or if the car has an engine smaller than 1000cc.
+
+function canDriveCar(user,car){
+
+}
